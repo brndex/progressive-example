b07cf920626849a4aa0029e0f6676796b415fbb5
brndex <brndexpro@gmail.com>
2017-01-17 01:51:54 -0800

    first commit

---
+++ js/template.js
@@ -0,0 +1,32 @@
+define([], function(){
+
+    function generateCarCard(car){
+      var template = document.querySelector('#car-card').innerHTML;
+      var title = car.brand + ' ' + car.model + ' ' + car.year;
+      template = template.replace("{{title}}", title);
+      template = template.replace('{{details-id}}', car.details_id);
+      template = template.replace('{{image}}', car.image);
+      template = template.replace('{{price}}', car.price);
+      return template;
+    };
+
+    function appendCars(cars){
+        document.getElementById('first-load').innerHTML = "";
+        var cardHTML = "";
+        for(var i = 0; i < cars.length; i++){
+            if(cars[i].value) {
+                cars[i] = cars[i].value;
+            }
+            cardHTML += generateCarCard(cars[i]);
+        }
+        document.querySelector('.mdl-grid').insertAdjacentHTML('beforeend', cardHTML);
+        //Force Redraw Fix for IE
+        document.querySelector('.mdl-layout__content').style.display = 'none';
+        document.querySelector('.mdl-layout__content').style.display = "inline-block";
+    }
+
+    return {
+        appendCars: appendCars
+    };
+
+});
